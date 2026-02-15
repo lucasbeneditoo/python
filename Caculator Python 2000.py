7764401144ed55388fd4d6ae2852b5abda822035
mentalze <lucasbenedito1020@gmail.com>
2026-02-15 19:09:27 -0300

    Fix spelling errors and improve code structure in Calculator Python 2000

--- Caculator Python 2000.py
+++ Caculator Python 2000.py
@@ -1,33 +1,40 @@
-print("Bem-Vindo ao Caculator Python 2000!")
+print("Bem-Vindo ao Calculator Python 2000!")
 
-print("1 - Somar")
-print("2 - Subtrair")
-print("3 - Multiplicar")
-print("4 - Dividir")
+while True:
+    print("1 - Somar")
+    print("2 - Subtrair")
+    print("3 - Multiplicar")
+    print("4 - Dividir")
+    print("0 - Sair")
 
-escolha = int(input("Escolha uma opção digitando seu número: "))
+    escolha = int(input("Escolha uma opção digitando seu número: "))
 
-if escolha == 1:
-    val1 = float(input("Coloque um primeiro número para somar: "))
-    val2 = float(input("Coloque um segundo número para somar: "))
-    print("A soma dos dois números é igual a:", val1 + val2)
+    if escolha == 1:
+        val1 = float(input("Coloque um primeiro número para somar: "))
+        val2 = float(input("Coloque um segundo número para somar: "))
+        print("A soma dos dois números é igual a:", val1 + val2)
 
-elif escolha == 2:
-    val1 = float(input("Coloque um primeiro número para subtrair: "))
-    val2 = float(input("Coloque um segundo número para subtrair: "))
-    print("A subtração dos dois números é igual a:", val1 - val2)
+    elif escolha == 2:
+        val1 = float(input("Coloque um primeiro número para subtrair: "))
+        val2 = float(input("Coloque um segundo número para subtrair: "))
+        print("A subtração dos dois números é igual a:", val1 - val2)
 
-elif escolha == 3:
-    val1 = float(input("Coloque um primeiro número para multiplicar: "))
-    val2 = float(input("Coloque um segundo número para multiplicar: "))
-    print("A multiplicação dos dois números é igual a:", val1 * val2)
+    elif escolha == 3:
+        val1 = float(input("Coloque um primeiro número para multiplicar: "))
+        val2 = float(input("Coloque um segundo número para multiplicar: "))
+        print("A multiplicação dos dois números é igual a:", val1 * val2)
 
-elif escolha == 4:
-    val1 = float(input("Coloque um primeiro número para dividir: "))
-    val2 = float(input("Coloque um segundo número para dividir: "))
-    if val2 != 0:
-        print("A Dividisão dos dois números é igual a:", val1 / val2)
+    elif escolha == 4:
+        val1 = float(input("Coloque um primeiro número para dividir: "))
+        val2 = float(input("Coloque um segundo número para dividir: "))
+        if val2 != 0:
+            print("A Divisão dos dois números é igual a:", val1 / val2)
+        else:
+            print("Não se pode dividir por zero!")
+
+    elif escolha == 0:
+        print("Você saiu do programa.")
+        break
+    
     else:
-        print("Não se pode dividir por zero!")
-else:
-    print("Opção Inválida!")
+        print("Opção Inválida!")
