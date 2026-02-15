0e3211c59cc809acd420df7583d793764f1034dd
mentalze <lucasbenedito1020@gmail.com>
2026-02-15 19:38:58 -0300

    Refactor calculator input handling to improve user experience and error management

--- Caculator Python 2000.py
+++ Caculator Python 2000.py
@@ -1,4 +1,4 @@
-print("Bem-Vindo ao Calculator Python 2000!")
+print("Bem-Vindo ao Calculator Python 2000!\n")
 
 while True:
     print("1 - Somar")
@@ -6,35 +6,71 @@ while True:
     print("3 - Multiplicar")
     print("4 - Dividir")
     print("0 - Sair")
+    print()
 
     escolha = int(input("Escolha uma opção digitando seu número: "))
+    print()
+    if escolha == 1:   
+        while True:
+            try:
+                val1 = float(input("Coloque um primeiro número para somar: ")) 
+                print()
+                val2 = float(input("Coloque um segundo número para somar: "))
+                print()
+                print("A soma dos dois números é igual a:", val1 + val2)
+                print()
+                break
 
-    if escolha == 1:
-        val1 = float(input("Coloque um primeiro número para somar: "))
-        val2 = float(input("Coloque um segundo número para somar: "))
-        print("A soma dos dois números é igual a:", val1 + val2)
+            except ValueError:
+                print("Valor inválido! Tente novamente.\n")
 
     elif escolha == 2:
-        val1 = float(input("Coloque um primeiro número para subtrair: "))
-        val2 = float(input("Coloque um segundo número para subtrair: "))
-        print("A subtração dos dois números é igual a:", val1 - val2)
+        while True:
+            try:
+                val1 = float(input("Coloque um primeiro número para subtrair: "))
+                print()
+                val2 = float(input("Coloque um segundo número para subtrair: "))
+                print()
+                print("A subtração dos dois números é igual a:", val1 - val2)
+                print()
+                break
+
+            except ValueError:
+                print("Valor inválido! Tente novamente.\n")
 
     elif escolha == 3:
-        val1 = float(input("Coloque um primeiro número para multiplicar: "))
-        val2 = float(input("Coloque um segundo número para multiplicar: "))
-        print("A multiplicação dos dois números é igual a:", val1 * val2)
+        while True:
+            try:
+                val1 = float(input("Coloque um primeiro número para multiplicar: "))
+                print()
+                val2 = float(input("Coloque um segundo número para multiplicar: "))
+                print()
+                print("A multiplicação dos dois números é igual a:", val1 * val2)
+                print()
+                break
+
+            except ValueError:
+                print("Valor inválido! Tente novamente.\n")
 
     elif escolha == 4:
-        val1 = float(input("Coloque um primeiro número para dividir: "))
-        val2 = float(input("Coloque um segundo número para dividir: "))
-        if val2 != 0:
-            print("A Divisão dos dois números é igual a:", val1 / val2)
-        else:
-            print("Não se pode dividir por zero!")
+        while True:
+            try:
+                val1 = float(input("Coloque um primeiro número para dividir: "))
+                print()
+                val2 = float(input("Coloque um segundo número para dividir: "))
+                print()
+                if val2 != 0:
+                    print("A Divisão dos dois números é igual a:", val1 / val2)
+                    print()
+                    break
+                else:
+                    print("Não se pode dividir por zero!\n")
+            except ValueError:
+                print("Valor inválido! Tente novamente.\n")
 
     elif escolha == 0:
         print("Você saiu do programa.")
         break
     
     else:
-        print("Opção Inválida!")
+        print("Opção Inválida!\n")
